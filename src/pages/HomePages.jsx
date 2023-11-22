f7713ba44ed918436cfb31822569dc0e974f7024
YinwolfB <comandoxrp@hotmal.com>
2023-11-22 10:45:10 -0500

    Se inicia con la creacion del store

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,6 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { useRef } from "react"
 
 export const HomePages = () => {
+
+    const inputName = useRef()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        inputName.current.value.trim()
+    }
+
     return (
-        <div>HomePages</div>
+        <div>
+            <h1>Pokedex</h1>
+            <h2>Hi Trainer!</h2>
+            <p>To start, please give me your trainer name</p>
+            <form onSubmit={handleSubmit}>
+                <input ref={inputName} type="text" />
+                <button>Gotta catch'em all!</button>
+            </form>
+        </div>
     )
 }
